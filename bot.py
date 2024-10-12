82b78abbc1f4bd212afd0696e4fea677733ab411
shampun511 <den-belov00@mail.ru>
2024-10-12 12:51:17 +0500

    1

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import config
+from random import choice
 
-import config
-
+import Telebot
 import asyncio
 
-from telebot.async_telebot import AsyncTeleBot
+from Telebot.async_telebot import AsyncTeleBot
 
 bot = AsyncTeleBot(config.token)
 
-
+banned_users = set()
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
@@ -22,7 +22,30 @@ async def send_hello(message):
 @bot.message_handler(commands=['joke'])
 async def send_joke(message):
     anekdot = choice(['колобок повесился', "русалка села на шпагат"])
+
     bot.reply_to(message, anekdot)
+
+@bot.message_handler(user_id=banned_users)
+async def handle_banned(msg: Message):
+    print(f"{msg.from_user.full_name} пишет, но мы ему не ответим!")
+    return True
+
+
+@bot.message_handler()
+async def handle_all(msg: Message):
+    await msg.reply(f"Добрый день, {msg.from_user.full_name} :)")
+
+
+@bot.message_handler(commands=['ban'], user_id=12312312312312) # здесь укажи свой ID
+async def handle_ban_command(msg: Message):
+    # проверяем, что ID передан правильно
+    try:
+        abuser_id = int(msg.get_args())
+    except (ValueError, TypeError):
+        return await msg.reply("Укажи ID пользователя.")
+    
+    banned_users.add(abuser_id)
+    await msg.reply(f"Пользователь {abuser_id} заблокирован.")
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
